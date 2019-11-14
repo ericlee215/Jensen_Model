2123cae3526b77b551ab65e215366060df2e8fb3
ericlee215 <51139205+ericlee215@users.noreply.github.com>
2019-11-13 20:48:12 -0700

    Add files via upload

---
+++ Katic_Model2.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 11:40:30 2019
+
+@author: Owner
+"""
+import numpy as np
+from Jensen_Point_Model import Jensen_Point
+#from Tester import rotation_matrix as rm
+
+def Katic(TurbineN, rnot, PosWind,alpha,u,otheru):
+    
+    n = len(TurbineN)
+    WSN = np.zeros([n,2])
+
+    for i in range(n):
+        
+        WSN[i,:] = Jensen_Point(alpha,rnot,otheru[0,i],TurbineN[i,:],PosWind)
+    
+   
+    WSNT = WSN[:,0]
+    WSNC = WSN[:,1]
+    print(WSNT)
+    print(WSNC)
+    print(otheru)
+        
+    if sum(WSNT == u) == n-1:
+        
+        WS1 = WSNT[WSNT.argmin()]
+        
+    elif sum(WSNT == u) == n:
+        WS1 = u
+        
+    else:
+        tin =0
+        for i in range(n):
+            tin = (1 - WSNT[i]/otheru[0,i])**2  + tin
+            print(tin)
+        WS1 = u * (1 - np.sqrt(tin))
+        print(WS1)
+       
+    if sum(WSNC == u) == n-1:
+        
+        WS2 = WSNC[WSNC.argmin()]
+        
+    elif sum(WSNC == u) == n:
+        WS2 = u
+        
+    else:
+        tin = 0
+        for i in range(n):
+            tin = (1 - WSNC[i]/otheru[0,i])**2  + tin
+        WS2 = u * (1 - np.sqrt(tin))
+        
+    WindSpeed = np.array([WS1,WS2])
+    
+    return WindSpeed
